ff2cd6d5dff82469d778095999dae6b8f572efc6
GGuedesAB <gustavoguedesab@gmail.com>
2019-11-03 15:36:51 -0300

    Making Money an abstract factory and adding tests

--- TDD/src/tdd/Dollar.java
+++ TDD/src/tdd/Dollar.java
@@ -1,15 +1,10 @@
 package tdd;
 
 class Dollar extends Money {
-    public Dollar(int amount) {
+    Dollar(int amount) {
         this.amount = amount;
     }
-    public Dollar times(int multiplier) {
+    Money times(int multiplier) {
         return new Dollar (amount * multiplier);
-    }
-    @Override
-    public boolean equals(Object object){
-        Dollar dollar = (Dollar) object;
-        return amount == dollar.amount;
     }
 }
--- TDD/src/tdd/Franc.java
+++ TDD/src/tdd/Franc.java
@@ -1,15 +1,10 @@
 package tdd;
 
 class Franc extends Money {
-    public Franc(int amount) {
+    Franc(int amount) {
         this.amount = amount;
     }
-    Franc times(int multiplier) {
+    Money times(int multiplier) {
         return new Franc (amount * multiplier);
-    }
-    @Override
-    public boolean equals(Object object) {
-        Franc franc = (Franc) object;
-        return amount == franc.amount;
     }
 }
